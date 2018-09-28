62dbdfb0879054352e732c7fbbd3c2588e4b90f9
赵珂飞 <546109656@qq.com>
2018-09-28 22:48:58 +0800

    fix bug

--- wechat/urls.py
+++ wechat/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 from werobot.contrib.django import make_view
 from werobot import WeRoBot
 
+from wechat.views import werobot_view
+
 robot = WeRoBot(
     token='feifeiwechat',
     app_id='wxde12b488de883dab',
     app_secret='7b638241ad308dab37f1f2fe7ea97103')
 
 urlpatterns = [
-    url(r'^$', make_view(robot), name='robot')
+    url(r'^$', werobot_view, name='robot')
 ]
--- wechat/views.py
+++ wechat/views.py
@@ -2,8 +2,42 @@
 from __future__ import unicode_literals
 
 # Create your views here.
+import html
+
+from django.http import HttpResponseForbidden, HttpResponse, HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
+
+from wechat.urls import robot
 
 
+@csrf_exempt
+def werobot_view(request):
+    timestamp = request.GET.get("timestamp", "")
+    nonce = request.GET.get("nonce", "")
+    signature = request.GET.get("signature", "")
+
+    if not robot.check_signature(
+            timestamp=timestamp, nonce=nonce, signature=signature
+    ):
+        return HttpResponseForbidden(
+            robot.make_error_page(
+                html.escape(request.build_absolute_uri())
+            )
+        )
+    if request.method == "GET":
+        return HttpResponse(request.GET.get("echostr", ""))
+    elif request.method == "POST":
+        message = robot.parse_message(
+            request.body,
+            timestamp=timestamp,
+            nonce=nonce,
+            msg_signature=request.GET.get("msg_signature", "")
+        )
+        return HttpResponse(
+            robot.get_encrypted_reply(message),
+            content_type="application/xml;charset=utf-8"
+        )
+    return HttpResponseNotAllowed(['GET', 'POST'])
 
 
 
