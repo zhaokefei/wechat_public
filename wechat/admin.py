639479aecddf8d3eede16c2921ca018c164c7427
赵珂飞 <546109656@qq.com>
2018-09-28 22:39:54 +0800

    fix bug

--- wechat/admin.py
+++ wechat/admin.py
@@ -2,12 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
 from wechat.models import Account
 
 
 class AccountAdmin(admin.ModelAdmin):
-    form = Account
     # list页面要显示的字段
     list_display = ['name', 'open_id']
 
